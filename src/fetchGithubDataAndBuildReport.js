fa4fe4d4fccd0c9fbefe3e4084cbb2efa5645f4e
Michael Fields <michael.fields@optimizely.com>
2018-12-11 14:39:15 -0800

    separate core data fetch and report gen code from command line hanlding code

---
+++ src/fetchGithubDataAndBuildReport.js
@@ -0,0 +1,227 @@
+const octokit = require('@octokit/rest')();
+const generateKarmaReport = require('./generateKarmaReport');
+
+// wrap setTimeout in a Promise so we can 'await' a time delay
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const uniqueFilter = (v, i, a) => a.indexOf(v) === i;
+
+/**
+ * Flatten an array that has at most one level of nested array elements
+ * @param {Array(Array)} arrayOfArrays 
+ */
+function shallowFlatten(arrayOfArrays) {
+  return arrayOfArrays.reduce((acc, elem) => acc.concat(elem), []);
+}
+
+/**
+ * Fetch all octokit API results by fetching all pages until complete
+ * @param {function} method 
+ * @param {Array} queryProperties 
+ */
+async function paginate(method, queryProperties = {}, logger) {
+  const queryObject = {
+    per_page: 50,
+  };
+
+  // apply any additional query properties
+  if (Object.keys(queryProperties)) {
+    Object.assign(queryObject, queryProperties)
+  }
+
+  try {
+    const options = method.endpoint.merge(queryObject)
+    const data = await octokit.paginate(
+      options,
+      (response) => {
+        return (method === octokit.search.issues) ? response.data.items : response.data
+      }
+    );
+    return data;
+  } catch(e) {
+    if (e.message) {
+      if (e.message.documentation_url && e.message.documentation_url.includes('abuse-rate-limits')) {
+        logger('Use of this script has triggered Github\'s abuse detection mechanism due to too many API requests being sent. Please wait a few minutes and try again.');        
+      } else {
+        logger(`Github Error: ${e.message}`);
+      }
+      process.exit(1);
+    }
+  }
+}
+
+/**
+ * Filter out files that should not contribute to the karma score
+ * @param {*} files 
+ */
+function filterFiles(files) {
+  const excludeRegexes = [
+    /^.*yarn[-,\.]|package-lock\.json.*$/, // omit changes to yarn/npm lock and error log files
+  ]
+  return files.filter(
+    file => !excludeRegexes.some(regex => file.filename.match(regex))
+  );
+}
+
+function mergeKarmaScoreMaps(karmaScoreMaps) {
+  return karmaScoreMaps.reduce( 
+    (mergedScores, scoreMap) => {
+      Object.keys(scoreMap).forEach(reviewer => {
+        const currentScore = mergedScores[reviewer];
+        if (currentScore) {
+          mergedScores[reviewer] += scoreMap[reviewer];
+        } else {
+          mergedScores[reviewer] = scoreMap[reviewer];
+        }
+      });
+      return mergedScores;
+    },
+    {}
+  );
+}
+
+async function fetchGithubDataAndBuildReport({
+  githubAccessToken,
+  logger,
+  githubOrg,
+  githubTeams,
+  daysToSearch,
+  karmaPerReview,
+  karmaPercentPerComment
+}) {
+  const mergedSinceDate = new Date();
+  mergedSinceDate.setDate(mergedSinceDate.getDate() - daysToSearch);
+
+  octokit.authenticate({
+    type: 'oauth',
+    token: githubAccessToken,
+  });
+
+  const teams = await paginate(
+    octokit.teams.list,
+    { org: githubOrg },
+    logger
+  );
+
+  const teamIds = teams.filter(team => githubTeams.includes(team.name)).map(team => team.id);
+
+  const usersPerTeamList = await Promise.all(teamIds.map(async (teamId) => {
+    return await paginate(
+      octokit.teams.listMembers,
+      {
+        team_id: teamId,
+        org: githubOrg,
+      },
+      logger,
+    );
+  }));
+  // flatten the per-team users arrays to a single array
+  const teamUsers = shallowFlatten(usersPerTeamList);
+
+  const teamUserLogins = teamUsers.map(user => user.login);
+
+  // get repositories associated with github teams(s)
+  const teamRepositories = shallowFlatten(
+    await Promise.all(teamIds.map(async (teamId) => {
+      return await paginate(
+        octokit.teams.listRepos,
+        {
+          team_id: teamId,
+          org: githubOrg,
+        },
+        logger,
+      );
+    }))
+  ).map(repository => repository.name)
+    .filter(uniqueFilter);
+
+  const delayPromises = [];
+  let pullRequestCount = 0;
+
+  // fetch pull request data for each repository
+  const repoPromises = teamRepositories.map(async (repository, repoIndex) => {
+
+    const pullRequests = await paginate(
+      octokit.search.issues,
+      {
+        q: `repo:${githubOrg}/${repository} is:pr merged:>=${mergedSinceDate.toISOString().slice(0, 10)} -author:optibot-cd`,
+      },
+      logger,
+    );
+
+    pullRequestCount += pullRequests.length;
+    
+    pullRequests.forEach((pullRequest, prIndex) => {
+      const delayPromise = (async () => {
+        // add some variable latency to each batch of API requests to avoid trigerring github API abuse detection
+        await timeout(5 + 10 * prIndex + 100 * repoIndex); 
+    
+        const prQuery = {
+          owner: githubOrg,
+          repo: repository,
+          number: pullRequest.number,
+        };
+        return [
+          // API request to fetch file details for this pull request
+          paginate(octokit.pullRequests.listFiles, prQuery, logger),
+          // API request to fetch reviews for this pull request
+          paginate(octokit.pullRequests.listReviews, prQuery, logger),
+          // Save a reference to the pull request author
+          Promise.resolve(pullRequest.user.login),
+        ];
+      })();
+      delayPromises.push(delayPromise);
+      return;
+    });
+    
+  });
+
+  await Promise.all(repoPromises);
+  const promiseArrays = await Promise.all(delayPromises);
+
+  // Process each pull request for per-user code review karma
+  const karmaPerPullRequestMaps = await Promise.all(promiseArrays.map(async (promiseArray) => {
+    const [files, reviews, author] = await Promise.all(promiseArray);
+    const additionsReducer = (acc, file) => (acc + file.additions);
+    const allFileAdditions = filterFiles(files).reduce(additionsReducer, 0);
+    const approvingReviewers = reviews
+      .filter(review => review.state === 'APPROVED') // reviewers that actually approved the PR
+      .map(review => review.user.login)  // map the reviews to the reviewers' github account names
+      .filter(uniqueFilter) // eliminate duplicate reviewers (if same user reviewed the PR mutiple times)
+      .filter(reviewer => teamUserLogins.includes(reviewer)); // only consider reviews from users in the team we are analyzing
+
+    const commentingReviewers = reviews
+      .filter(review => review.state !== 'APPROVED') // reviewers that commented on the PR
+      .map(review => review.user.login)  // map the reviews to the reviewers' github account names
+      .filter(uniqueFilter) // eliminate duplicate reviewers (if same user reviewed the PR mutiple times)
+      .filter(
+        reviewer =>
+          teamUserLogins.includes(reviewer) && // only consider reviews from users in the team we are analyzing
+          !approvingReviewers.includes(reviewer) && // discard comment reviews from an approving reviewer
+          reviewer !== author // exclude comments made by the PR author
+      );
+
+    // determine karma scores for this PR
+    const karmaPerPullRequestMap = {};
+    approvingReviewers.forEach((reviewer) => {
+      karmaPerPullRequestMap[reviewer] = karmaPerReview + allFileAdditions;
+    });
+    commentingReviewers.forEach((reviewer) => {
+      karmaPerPullRequestMap[reviewer] = Math.trunc((karmaPercentPerComment / 100) * allFileAdditions);
+    });
+
+    return karmaPerPullRequestMap;
+  }));
+
+  return [
+    generateKarmaReport(mergeKarmaScoreMaps(karmaPerPullRequestMaps)),
+    {
+      pullRequestCount,
+      reviewers: teamUserLogins,
+    }
+  ];
+}
+
+module.exports = fetchGithubDataAndBuildReport;
--- src/scripts/code-review-karma-report.js
+++ src/scripts/code-review-karma-report.js
@@ -1,4 +1,4 @@
-const octokit = require('@octokit/rest')();
+const fetchGithubDataAndBuildReport = require('../fetchGithubDataAndBuildReport');
 
 /*
  * This script uses the github API to inspect recently merged frontend pull requests
@@ -25,24 +25,6 @@ const colors = {
   default: '\x1b[0m',
 }
 
-const mergedSinceDate = new Date();
-mergedSinceDate.setDate(mergedSinceDate.getDate() - DAYS_TO_SEARCH);
-
-// wrap setTimeout in a Promise so we can 'await' a time delay
-function timeout(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-const uniqueFilter = (v, i, a) => a.indexOf(v) === i;
-
-/**
- * Flatten an array that has at most one level of nested array elements
- * @param {Array(Array)} arrayOfArrays 
- */
-function shallowFlatten(arrayOfArrays) {
-  return arrayOfArrays.reduce((acc, elem) => acc.concat(elem), []);
-}
-
 /**
  * Render text with specified color on the terminal
  * @param {string} string 
@@ -52,229 +34,47 @@ function withColor(string, color) {
   return `${colors[color]}${string}${colors.default}`;
 }
 
-let apiRequestCount = 0;
-
 /**
- * Fetch all octokit API results by fetching all pages until complete
- * @param {function} method 
- * @param {Array} queryProperties 
+ * Display the Code Review Karma report to the console
+ * @param {Object} karmaReportArrays 
  */
-async function paginate(method, queryProperties = {}) {
-  const queryObject = {
-    per_page: 50,
-  };
-
-  // apply any additional query properties
-  if (Object.keys(queryProperties)) {
-    Object.assign(queryObject, queryProperties)
-  }
-
-  try {
-    const options = method.endpoint.merge(queryObject)
-    const data = await octokit.paginate(
-      options,
-      (response) => {
-        apiRequestCount += 1;
-        return (method === octokit.search.issues) ? response.data.items : response.data
-      }
-    );
-    return data;
-  } catch(e) {
-    if (e.message) {
-      if (e.message.documentation_url && e.message.documentation_url.includes('abuse-rate-limits')) {
-        console.log('Use of this script has triggered Github\'s abuse detection mechanism due to too many API requests being sent. Please wait a few minutes and try again.');        
-      } else {
-        console.log(`Github Error: ${e.message}`);
-      }
-      process.exit(1);
-    }
-  }
-}
-
-/**
- * Filter out files that should not contribute to the karma score
- * @param {*} files 
- */
-function filterFiles(files) {
-  const excludeRegexes = [
-    /^.*yarn[-,\.]|package-lock\.json.*$/, // omit changes to yarn/npm lock and error log files
-  ]
-  return files.filter(
-    file => !excludeRegexes.some(regex => file.filename.match(regex))
-  );
-}
-
-const reviewerKarmaScores = {};
-
-/**
- * Generate the Code Review Karma report
- * @param {Array(Object)} karmaPerPullRequestMaps 
- */
-function generateReport(karmaPerPullRequestMaps) {
-  const reviewersCount = Object.keys(reviewerKarmaScores).length;
-  const reviewersCountText = withColor(reviewersCount, 'magenta');
-  const daysCountText = withColor(DAYS_TO_SEARCH.toString(), 'magenta');
-  console.log(`Preparing code review karma report (${reviewersCountText} team reviewers over ${daysCountText} days) ...`);
-  const sortableKarmaScores = [];
-  let sumOfAllScores = 0;
-  for (const reviewer in reviewerKarmaScores) {
-    sortableKarmaScores.push([reviewer, reviewerKarmaScores[reviewer]]);
-    sumOfAllScores += reviewerKarmaScores[reviewer];
-  }
-  const averageScore = Math.trunc(sumOfAllScores / reviewersCount);
-  const longestReviewer = Math.max(12, ...sortableKarmaScores.map(karmaScore => karmaScore[0].length));
-  const longestScore = Math.max(...sortableKarmaScores.map(karmaScore => karmaScore[1].toString().length));
-  sortableKarmaScores.sort((a, b) => b[1] - a[1]);
+function displayConsoleReport(karmaReportArrays) {
+  const longestReviewer = Math.max(12, ...karmaReportArrays.map(karmaScore => karmaScore[0].length));
+  const longestScore = Math.max(...karmaReportArrays.map(karmaScore => karmaScore[1].toString().length));
   const horizontalRule = `--${''.padEnd(longestReviewer, '-')}----------------------------`;
   console.log(horizontalRule);
   console.log(`| ${withColor('Reviewer'.padEnd(longestReviewer), 'cyan')} | ${withColor('Karma Score', 'cyan')} | ${withColor('% of Avg.', 'cyan')} |`);
   console.log(horizontalRule);
-  sortableKarmaScores.forEach(([reviewer, karmaScore]) => {
+  karmaReportArrays.forEach(([reviewer, karmaScore, percentOfAverage]) => {
     const reviewerText = withColor(reviewer.padEnd(longestReviewer), 'green');
     const scoreText = withColor(karmaScore.toString().padStart(longestScore + 7), 'yellow');
-    const percentText = withColor(Math.trunc(100 * karmaScore / averageScore).toString().padStart(9), 'yellow');
+    const percentText = withColor(percentOfAverage.toString().padStart(9), 'yellow');
     console.log(`| ${reviewerText} | ${scoreText} | ${percentText} |`);
   })
   console.log(horizontalRule);
-  // console.log(`[DEBUG] Issued ${withColor(apiRequestCount, 'magenta')} github API requests, average score = ${averageScore}`);
 }
 
-async function main() {
-  if (process.env.GITHUB_ACCESS_TOKEN) {
-    octokit.authenticate({
-      type: 'oauth',
-      token: process.env.GITHUB_ACCESS_TOKEN,
-    });
-  } else {
-    console.log('You must populate a github personal access token in the GITHUB_ACCESS_TOKEN env variable in order to use this script.');
-    process.exit(1);
-  }
-
-  console.log(`Fetching all ${withColor(ORG_NAME, 'cyan')} org teams ...`);
-  const teams = await paginate(
-    octokit.teams.list,
-    { org: ORG_NAME },
-  );
-
-  console.log(`Found ${withColor(teams.length, 'magenta')} org teams ...`);
-  const teamIds = teams.filter(team => TEAM_NAMES.includes(team.name)).map(team => team.id);
-
-  const usersPerTeamList = await Promise.all(teamIds.map(async (teamId) => {
-    console.log(`Fetching ${withColor(teams.find(team => team.id === teamId).name, 'cyan')} team users ...`);
-    return await paginate(
-      octokit.teams.listMembers,
-      {
-        team_id: teamId,
-        org: ORG_NAME,
-      },
-    );
-  }));
-  // flatten the per-team users arrays to a single array
-  const teamUsers = shallowFlatten(usersPerTeamList);
-
-  console.log(`Found ${withColor(teamUsers.length, 'magenta')} teams users ...`);
-  const teamUserLogins = teamUsers.map(user => user.login);
-
-  // seed the karma scores with 0 for each team member
-  teamUserLogins.forEach(reviewer => {
-    reviewerKarmaScores[reviewer] = 0;
-  });
-
-  // get repositories associated with github teams(s)
-  const teamRepositories = shallowFlatten(
-    await Promise.all(teamIds.map(async (teamId) => {
-      console.log(`Fetching ${withColor(teams.find(team => team.id === teamId).name, 'cyan')} team repositories ...`);
-      return await paginate(
-        octokit.teams.listRepos,
-        {
-          team_id: teamId,
-          org: ORG_NAME,
-        },
-      );
-    }))
-  ).map(repository => repository.name)
-    .filter(uniqueFilter);
-
-  const delayPromises = [];
-
-  // fetch pull request data for each repository
-  const repoPromises = teamRepositories.map(async (repository, repoIndex) => {
-    console.log(`Fetching pull requests for repository ${withColor(repository, 'cyan')} ...`);
-
-    const pullRequests = await paginate(
-      octokit.search.issues,
-      {
-        q: `repo:${ORG_NAME}/${repository} is:pr merged:>=${mergedSinceDate.toISOString().slice(0, 10)} -author:optibot-cd`,
-      }
-    );
-    
-    pullRequests.forEach((pullRequest, prIndex) => {
-      const delayPromise = (async () => {
-        // add some variable latency to each batch of API requests to avoid trigerring github API abuse detection
-        await timeout(5 + 10 * prIndex + 100 * repoIndex); 
-    
-        // console.log(`PR: ${pullRequest.title}`);
-        const prQuery = {
-          owner: ORG_NAME,
-          repo: repository,
-          number: pullRequest.number,
-        };
-        return [
-          // API request to fetch file details for this pull request
-          paginate(octokit.pullRequests.listFiles, prQuery),
-          // API request to fetch reviews for this pull request
-          paginate(octokit.pullRequests.listReviews, prQuery),
-          // Save a reference to the pull request author
-          Promise.resolve(pullRequest.user.login),
-        ];
-      })();
-      delayPromises.push(delayPromise);
-      return;
-    });
-    
-  });
-
-  await Promise.all(repoPromises);
-  const promiseArrays = await Promise.all(delayPromises);
-
-  // Process each pull request for per-user code review karma
-  const karmaPerPullRequestMaps = await Promise.all(promiseArrays.map(async (promiseArray) => {
-    const [files, reviews, author] = await Promise.all(promiseArray);
-    const additionsReducer = (acc, file) => (acc + file.additions);
-    const additionsKarma = filterFiles(files).reduce(additionsReducer, 0);
-    const approvingReviewers = reviews
-      .filter(review => review.state === 'APPROVED') // reviewers that actually approved the PR
-      .map(review => review.user.login)  // map the reviews to the reviewers' github account names
-      .filter(uniqueFilter) // eliminate duplicate reviewers (if same user reviewed the PR mutiple times)
-      .filter(reviewer => teamUserLogins.includes(reviewer)); // only consider reviews from users in the team we are analyzing
-
-    const commentingReviewers = reviews
-      .filter(review => review.state !== 'APPROVED') // reviewers that commented on the PR
-      .map(review => review.user.login)  // map the reviews to the reviewers' github account names
-      .filter(uniqueFilter) // eliminate duplicate reviewers (if same user reviewed the PR mutiple times)
-      .filter(
-        reviewer =>
-          teamUserLogins.includes(reviewer) && // only consider reviews from users in the team we are analyzing
-          !approvingReviewers.includes(reviewer) && // discard comment reviews from an approving reviewer
-          reviewer !== author // exclude comments made by the PR author
-      );
-
-    // determine karma scores for this PR
-    approvingReviewers.forEach((reviewer) => {
-      const currentKarma = reviewerKarmaScores[reviewer] || 0;
-      const updatedKarma = currentKarma + KARMA_PER_REVIEW + additionsKarma;
-      reviewerKarmaScores[reviewer] = updatedKarma;
-    });
-    commentingReviewers.forEach((reviewer) => {
-      const currentKarma = reviewerKarmaScores[reviewer] || 0;
-      const updatedKarma = currentKarma + Math.trunc((KARMA_PERCENT_PER_COMMENT / 100) * additionsKarma);
-      reviewerKarmaScores[reviewer] = updatedKarma;
-    });
-
-    return;
-  }));
-  generateReport(karmaPerPullRequestMaps);
+if (!process.env.GITHUB_ACCESS_TOKEN) {
+  console.log('You must populate a github personal access token in the GITHUB_ACCESS_TOKEN env variable in order to use this script.');
+  process.exit(1);
 }
 
-// kick off the main script execution function
-main();
+console.log(`Calculating Code Review Karma report for team(s): ${withColor(TEAM_NAMES.split(',').join(', '), 'cyan')} in the ${withColor(ORG_NAME, 'cyan')} org ...`)
+
+// kick off the main function to fetch data from github and generate the code review karma report
+fetchGithubDataAndBuildReport({
+  githubAccessToken: process.env.GITHUB_ACCESS_TOKEN,
+  logger: console.log,
+  githubOrg: ORG_NAME,
+  githubTeams: TEAM_NAMES,
+  daysToSearch: DAYS_TO_SEARCH,
+  karmaPerReview: KARMA_PER_REVIEW,
+  karmaPercentPerComment: KARMA_PERCENT_PER_COMMENT,
+}).then(([karmaReportData, statistics]) => {
+  console.log(`Code Review Karma report based on ${withColor(statistics.pullRequestCount, 'cyan')} reviewed pull requests over ${withColor(DAYS_TO_SEARCH, 'cyan')} days.`)
+  // display the karma report to the console
+  displayConsoleReport(karmaReportData);
+});
+
+
+

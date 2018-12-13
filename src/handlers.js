afe59f8a2c262bf0d0be562c1d68ddf1e0211846
Ryn830 <ryn830@gmail.com>
2018-12-12 22:17:06 -0800

    Confirm receipt and send notification of report generation

--- src/handlers.js
+++ src/handlers.js
@@ -31,42 +31,48 @@ module.exports.authorization = (event, context, callback) => {
 
 module.exports.report = (event, context, callback) => {
   // Confirm message was received
-  let promise = new Promise (resolve => {
-    callback(null, {
-      statusCode: 200,
-      body: {
-        text: 'Generating the karma report...'
-      },
-    })
+  callback(null, { statusCode: 200 })
 
-    // Parse the urlencoded body for the response_url
-    const payload = decodeURIComponent(event.body)
-      .split('&')
-      .map(field => field.split('='))
-      .reduce((index, pair) => {
-        index[pair[0]] = pair[1]
-        return index
-      }, {})
-    resolve(payload)
-  })
+  // Parse the urlencoded body for the response_url
+  const payload = decodeURIComponent(event.body)
+    .split('&')
+    .map(field => field.split('='))
+    .reduce((index, pair) => {
+      index[pair[0]] = pair[1]
+      return index
+    }, {})
 
   const KARMA_PER_REVIEW = 50
   const KARMA_PERCENT_PER_COMMENT = 25
   const DAYS_TO_REPORT = 30
-  promise.then(payload => {
-      return Promise.all([
-        fetchGithubDataAndBuildReport({
-          githubAccessToken: process.env.GITHUB_ACCESS_TOKEN,
-          logger: () => {},
-          githubOrg: process.env.GITHUB_ORG,
-          githubTeams: process.env.GITHUB_TEAMS,
-          daysToReport: DAYS_TO_REPORT,
-          karmaPerReview: KARMA_PER_REVIEW,
-          karmaPercentPerComment: KARMA_PERCENT_PER_COMMENT,
-        }),
-        payload
-      ])
-    })
+
+  request({
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    uri: payload.response_url,
+    json: true,
+    statusCode: 200,
+    body: {
+      "response_type": "in_channel",
+      "text": 'Generating karma report...',
+    },
+  })
+  .then(() => {
+    return Promise.all([
+      fetchGithubDataAndBuildReport({
+        githubAccessToken: process.env.GITHUB_ACCESS_TOKEN,
+        logger: () => {},
+        githubOrg: process.env.GITHUB_ORG,
+        githubTeams: process.env.GITHUB_TEAMS,
+        daysToReport: DAYS_TO_REPORT,
+        karmaPerReview: KARMA_PER_REVIEW,
+        karmaPercentPerComment: KARMA_PERCENT_PER_COMMENT,
+      }),
+      payload
+    ])
+  })
   .then(([response, payload]) => {
     const text = formatReportForSlack(response, DAYS_TO_REPORT)
     return request({

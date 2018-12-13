ff08299684c59fb4017c6f9b2cd5453d4ae4e58d
Michael Fields <michael.fields@optimizely.com>
2018-12-13 13:22:01 -0800

    pull intro text out of code block in slack output for improved formatting

--- src/formatReportForSlack.js
+++ src/formatReportForSlack.js
@@ -1,17 +1,20 @@
 module.exports = ([karmaReportArrays, statistics], daysToReport) => {
-  const teams = process.env.GITHUB_TEAMS.split(',').join(', ')
-  const organization = process.env.GITHUB_ORG
+  const teams = process.env.GITHUB_TEAMS.split(',');
+  const organization = process.env.GITHUB_ORG;
 
+  const displayTeams = teams.join(', ');
   const longestReviewer = Math.max(12, ...karmaReportArrays.map(karmaScore => karmaScore[0].length));
   const longestScore = Math.max(...karmaReportArrays.map(karmaScore => karmaScore[1].toString().length));
   const horizontalRule = `--${''.padEnd(longestReviewer, '-')}----------------------------\n`;
 
-  const title = `Code Review Karma report for team(s):\n${teams} in the ${organization} github org\n`
-  const subtitle = `based on ${statistics.pullRequestCount} reviewed pull requests over the past ${daysToReport} days:\n`
+  const title = `Code Review Karma report for team${ teams.length > 1 ? 's' : '' }:\n` +
+    `*${displayTeams}* in the *${organization}* github org.\n`
+  const subtitle = `Report based on *${statistics.pullRequestCount}* reviewed pull requests over the past *${daysToReport}* days:\n`
 
-  const report = "\n```" +
+  const report = "\n" +
     title +
     subtitle +
+    '```\n' +
     horizontalRule +
     `| ${'Reviewer'.padEnd(longestReviewer)} | Karma Score | % of Avg. |\n` +
     horizontalRule +
